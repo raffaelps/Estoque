4dd2941463d1330c6e9bb5d1c5f84d733f2f1133
Raffael Patrício de Souza <raffaelps@gmail.com>
2013-09-21 10:41:58 -0300

    Criação novo projeto

---
+++ Sistoque/SistoqueTests/SistoqueTests.m
@@ -0,0 +1,34 @@
+//
+//  SistoqueTests.m
+//  SistoqueTests
+//
+//  Created by Raffael Patrício de Souza on 21/09/13.
+//  Copyright (c) 2013 raffaelps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SistoqueTests : XCTestCase
+
+@end
+
+@implementation SistoqueTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
