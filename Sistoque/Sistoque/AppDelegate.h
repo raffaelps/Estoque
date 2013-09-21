e3101998e7f62a3e9f1db6c8360c0d00d06309cd
Raffael Patrício de Souza <raffaelps@gmail.com>
2013-09-21 10:46:37 -0300

    Novo projeto

---
+++ Sistoque/Sistoque/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sistoque
+//
+//  Created by Raffael Patrício de Souza on 21/09/13.
+//  Copyright (c) 2013 raffaelps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
