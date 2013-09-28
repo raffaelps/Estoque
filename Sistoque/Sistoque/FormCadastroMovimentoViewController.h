6015f56fa9e65f015a51c69f68eee5db0fe89d25
PedroFariasBarbosa <pedro.farias.barbosa@gmail.com>
2013-09-28 10:01:40 -0300

    Implementação .m Form Movimento

--- Sistoque/Sistoque/FormCadastroMovimentoViewController.h
+++ Sistoque/Sistoque/FormCadastroMovimentoViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface FormCadastroMovimentoViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *labelNroMovimento;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *segmentedTipoMovimento;
+@property (strong, nonatomic) IBOutlet UITextField *textProduto;
+@property (strong, nonatomic) IBOutlet UITextField *textDataMovimento;
+@property (strong, nonatomic) IBOutlet UITextField *qtdMovimento;
+@property (strong, nonatomic) IBOutlet UITextField *vlrMovimento;
+@property (strong, nonatomic) IBOutlet UISwitch *switchStatus;
 
+-(void)setNextMovimento:(int)nextMovimento;
+-(void)setProduto:(id)produto;
 @end
