ac03f6bbdafce6dd15bc83aa2564ac885e846e65
Raffael Patrício de Souza <raffaelps@gmail.com>
2013-09-28 11:33:13 -0300

    Lista de produtos

--- Sistoque/Sistoque/FormCadastroProdutoViewController.h
+++ Sistoque/Sistoque/FormCadastroProdutoViewController.h
@@ -8,6 +8,43 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FormCadastroProdutoViewController : UIViewController
+@interface FormCadastroProdutoViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
+{
+    IBOutlet UITableViewCell *cellNome;
+    IBOutlet UITableViewCell *cellCategoria;
+    IBOutlet UITableViewCell *cellDescricao;
+    IBOutlet UITableViewCell *cellValores;
+    IBOutlet UITableViewCell *cellQuantidades;
+    IBOutlet UITableViewCell *cellAtivo;
+    
+    IBOutlet UITextField *textNome;
+    IBOutlet UITextField *textCategoria;
+    IBOutlet UITextField *textDescricao;
+    IBOutlet UITextField *textValorEntrada;
+    IBOutlet UITextField *textValorSaida;
+    IBOutlet UITextField *textQuantMinima;
+    IBOutlet UITextField *textQuantMaxima;
+    IBOutlet UISwitch *ativo;
+    
+    IBOutlet UITableView *tableView;
+}
+
+@property(nonatomic,retain) IBOutlet UITableViewCell *cellNome;
+@property(nonatomic,retain) IBOutlet UITableViewCell *cellCategoria;
+@property(nonatomic,retain) IBOutlet UITableViewCell *cellDescricao;
+@property(nonatomic,retain) IBOutlet UITableViewCell *cellValores;
+@property(nonatomic,retain) IBOutlet UITableViewCell *cellQuantidades;
+@property(nonatomic,retain) IBOutlet UITableViewCell *cellAtivo;
+
+@property(nonatomic,retain) IBOutlet UITextField *textNome;
+@property(nonatomic,retain) IBOutlet UITextField *textCategoria;
+@property(nonatomic,retain) IBOutlet UITextField *textDescricao;
+@property(nonatomic,retain) IBOutlet UITextField *textValorEntrada;
+@property(nonatomic,retain) IBOutlet UITextField *textValorSaida;
+@property(nonatomic,retain) IBOutlet UITextField *textQuantMinima;
+@property(nonatomic,retain) IBOutlet UITextField *textQuantMaxima;
+@property(nonatomic,retain) IBOutlet UISwitch *ativo;
+
+@property(nonatomic,retain) IBOutlet UITableView *tableView;
 
 @end
--- Sistoque/Sistoque/FormCadastroProdutoViewController.m
+++ Sistoque/Sistoque/FormCadastroProdutoViewController.m
@@ -14,6 +14,8 @@
 
 @implementation FormCadastroProdutoViewController
 
+@synthesize cellAtivo,cellValores,cellQuantidades,cellDescricao,cellCategoria,cellNome,textCategoria,textDescricao,textNome,textQuantMaxima,textQuantMinima,textValorEntrada,textValorSaida,ativo,tableView;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -33,6 +35,12 @@
                                   target:self
                                   action:@selector(novoProduto)];
     self.navigationItem.rightBarButtonItem = addButton;
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,6 +52,72 @@
 - (IBAction)novoProduto
 {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 6;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        return cellNome;
+    }
+    if (indexPath.row == 1) {
+        return cellCategoria;
+    }
+    if (indexPath.row == 2) {
+        return cellDescricao;
+    }
+    if (indexPath.row == 3) {
+        return cellQuantidades;
+    }
+    if (indexPath.row == 4) {
+        return cellValores;
+    }
+    
+    return cellAtivo;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 3 || indexPath.row == 4) {
+        return 88;
+    }
+    
+    return 44;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self.tableView deselectRowAtIndexPath:[self.tableView indexPathForSelectedRow] animated:YES];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    UIEdgeInsets edge = UIEdgeInsetsMake(0, 0, -200, 0);
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.3];
+    self.tableView.contentInset = edge;
+    [UIView commitAnimations];
+	[textField resignFirstResponder];
+	return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    UIEdgeInsets edge = UIEdgeInsetsMake(0, 0, 200, 0);
+    self.tableView.contentInset = edge;
+    UITableViewCell *cell = (UITableViewCell *)[[textField superview] superview];
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+    [self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionTop animated:YES];
 }
 
 @end
