3abcc09402eb56b5ebaee7d4881f4bfe6655fdaf
Raffael Patrício de Souza <raffaelps@gmail.com>
2013-11-08 00:08:42 -0200

    Relatorios

--- Sistoque/Sistoque/ListaRelatorioViewController.m
+++ Sistoque/Sistoque/ListaRelatorioViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ListaRelatorioViewController.h"
+#import "RelatorioEntradaSaidaMesViewController.h"
 
 @interface ListaRelatorioViewController ()
 
@@ -18,7 +19,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        self.title = @"Relatórios";
     }
     return self;
 }
@@ -33,6 +34,59 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	static NSString *CelulaPrincipalCacheID = @"CelulaPrincipalCacheID";
+	UITableViewCell *cell = [tabela dequeueReusableCellWithIdentifier:CelulaPrincipalCacheID];
+    
+	if (!cell) {
+		cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:CelulaPrincipalCacheID];
+	}
+	
+    switch (indexPath.row) {
+        case 0:
+            cell.textLabel.text = @"Lista de produtos";
+            break;
+        case 1:
+            cell.textLabel.text = @"Saídas por mês";
+            break;
+        case 2:
+            cell.textLabel.text = @"Entradas por mês";
+            break;
+        default:
+            break;
+    }
+    
+    cell.textLabel.textColor = [UIColor grayColor];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+	
+	return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 1)
+    {
+        RelatorioEntradaSaidaMesViewController *relatorioView = [[RelatorioEntradaSaidaMesViewController alloc] init];
+        relatorioView.tipo = @"Saida";
+        [self.navigationController pushViewController:relatorioView animated:YES];
+    }
+    else if (indexPath.row == 2)
+    {
+        RelatorioEntradaSaidaMesViewController *relatorioView = [[RelatorioEntradaSaidaMesViewController alloc] init];
+        relatorioView.tipo = @"Entrada";
+        [self.navigationController pushViewController:relatorioView animated:YES];
+    }
+    
+    [tableView deselectRowAtIndexPath:[tableView indexPathForSelectedRow] animated:YES];
 }
 
 @end
