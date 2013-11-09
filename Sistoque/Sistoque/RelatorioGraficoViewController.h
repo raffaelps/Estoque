e87c5b81c642adcf927b6b8860c67fd8c48b4e2b
Raffael Patrício de Souza <raffaelps@gmail.com>
2013-11-09 00:19:39 -0200

    Relatorios

---
+++ Sistoque/Sistoque/RelatorioGraficoViewController.h
@@ -0,0 +1,13 @@
+//
+//  RelatorioGraficoViewController.h
+//  Sistoque
+//
+//  Created by Raffael Patrício de Souza on 08/11/13.
+//  Copyright (c) 2013 Leonardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RelatorioGraficoViewController : UIViewController
+
+@end
---
+++ Sistoque/Sistoque/RelatorioGraficoViewController.m
@@ -0,0 +1,38 @@
+//
+//  RelatorioGraficoViewController.m
+//  Sistoque
+//
+//  Created by Raffael Patrício de Souza on 08/11/13.
+//  Copyright (c) 2013 Leonardo. All rights reserved.
+//
+
+#import "RelatorioGraficoViewController.h"
+
+@interface RelatorioGraficoViewController ()
+
+@end
+
+@implementation RelatorioGraficoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
